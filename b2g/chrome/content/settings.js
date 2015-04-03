8c1561a631a2f2034bb7a8ce6c24318730d4160e
Julien Wajsberg <felash@gmail.com>
2015-04-03 11:13:42 +0200

    Bug 1111961 - follow-up

--- b2g/chrome/content/settings.js
+++ b2g/chrome/content/settings.js
@@ -546,7 +546,7 @@ let settingsToObserve = {
     prefName: 'nglayout.debug.paint_flashing',
     defaultValue: false
   },
-  'developer.mode' : false,
+  'developer.mode' : true,
   'devtools.eventlooplag.threshold': 100,
   'devtools.remote.wifi.visible': {
     resetToPref: true
